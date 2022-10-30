c1616deb8272e8b55a5b4faa552816387a81ac40
Olga Shiryavceva <olundel@yandex-team.ru>
2022-10-30 19:07:08 +0400

    added button on banner and fixed lettering

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:open_fashion/core/theme/colors_guide.dart';
-import 'package:open_fashion/presentation/utils/dimensions/dimension.dart';
 import 'package:open_fashion/presentation/widgets/base_app_bar.dart';
 import 'package:open_fashion/presentation/widgets/custom_scaffold.dart';
 import 'package:open_fashion/presentation/widgets/gap.dart';
@@ -28,8 +27,9 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     final List<String> tabs = [
-      'lib/assets/png/image_main.png',
-      'lib/assets/png/image_main.png',
+      'lib/assets/png/image_main1.png',
+      'lib/assets/png/image_main2.png',
+      'lib/assets/png/image_main3.png',
     ];
     return CustomScaffold(
       body: NestedScrollView(
@@ -51,10 +51,11 @@ class _HomePageState extends State<HomePage>
                     slivers: <Widget>[
                       SliverToBoxAdapter(
                         child: OnboardingBanner(
-                          screenWidth: 100.percentOfWidth,
-                          screenheight: 85.percentOfHeight,
                           image: pic,
-                          text: 'luxury fashion & accessories',
+                          count: tabs.length,
+                          firstText: 'luxury',
+                          secondText: 'fashion',
+                          thirdText: '&accessories',
                         ),
                       ),
                       const SliverToBoxAdapter(
--- lib/presentation/widgets/onboarding_banner.dart
+++ lib/presentation/widgets/onboarding_banner.dart
@@ -1,49 +1,74 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:open_fashion/core/theme/colors_guide.dart';
-import 'package:open_fashion/gen/fonts.gen.dart';
+import 'package:open_fashion/presentation/widgets/button_in.dart';
+
+import '../../core/theme/colors_guide.dart';
+import '../../gen/fonts.gen.dart';
 
 class OnboardingBanner extends StatelessWidget {
   const OnboardingBanner({
     Key? key,
-    required this.screenWidth,
-    required this.screenheight,
-    this.image,
-    this.text,
+    required this.image,
+    required this.firstText,
+    required this.secondText,
+    required this.thirdText,
+    required this.count,
   }) : super(key: key);
 
-  final double screenWidth;
-  final double screenheight;
-  final image;
-  final text;
+  final String image;
+  final int count;
+  final String firstText;
+  final String secondText;
+  final String thirdText;
 
   @override
   Widget build(BuildContext context) {
     //TODO need SliverStack
-    return Expanded(
-      child: Stack(
-        alignment: AlignmentDirectional.bottomStart,
-        children: <Widget>[
-          Image.asset(
-            image,
-            width: screenWidth,
-            height: screenheight,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            child: Expanded(
-              child: Text(
-                text.toString().toUpperCase(),
-                style: TextStyle(
-                  color: ColorsGuide.placeholder,
-                  fontSize: 28.0,
-                  fontFamily: FontFamily.bodoniModaBoldItalic,
-                ),
-              ),
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: <Widget>[
+        Image.asset(
+          image,
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+          left: 50,
+          top: 180,
+          child: Text(
+            firstText.toString().toUpperCase(),
+            style: TextStyle(
+              color: ColorsGuide.placeholder,
+              fontSize: 38.0,
+              fontFamily: FontFamily.bodoniModaBoldItalic,
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+        Positioned(
+          left: 80,
+          top: 220,
+          child: Text(
+            secondText.toString().toUpperCase(),
+            style: TextStyle(
+              color: ColorsGuide.placeholder,
+              fontSize: 38.0,
+              fontFamily: FontFamily.bodoniModaBoldItalic,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 40,
+          top: 260,
+          child: Text(
+            thirdText.toString().toUpperCase(),
+            style: TextStyle(
+              color: ColorsGuide.placeholder,
+              fontSize: 38.0,
+              fontFamily: FontFamily.bodoniModaBoldItalic,
+            ),
+          ),
+        ),
+        Positioned(left: 50, top: 400, child: ButtonIn()),
+      ],
     );
   }
 }
